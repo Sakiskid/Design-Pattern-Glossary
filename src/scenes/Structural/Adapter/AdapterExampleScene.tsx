8ca5d5787dbe103128d00b45df0b7812a9c7c4e0
Sakiskid <33964795+Sakiskid@users.noreply.github.com>
2024-02-12 23:22:08 -0600

    starts animation

--- src/scenes/Structural/Adapter/AdapterExampleScene.tsx
+++ src/scenes/Structural/Adapter/AdapterExampleScene.tsx
@@ -1,16 +1,37 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
+import {Circle, Icon, Txt, TxtProps, Node, Grid, makeScene2D} from '@motion-canvas/2d';
 import {all, createRef} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
     // Create your animations here
+    let bg = '#384046';
+    let primary = '#C5C739';
+    let secondary = '#3A8AC7';
+    let warning = '#C73D3A';
+    let dark = '#473938';
+    let light = '#717247';
+
+    // Background
+    view.fill(bg)
 
     const circle = createRef<Circle>();
+    const grid = createRef<Grid>();
 
-    view.add(<Circle ref={circle} size={320} fill={'lightseagreen'} />);
+    // view.add(<Icon icon={'f7:sportscourt'} size={320} color={primary}/>)
+    view.add(
+        <Grid ref={grid}>
+            <Txt position={[0, -500]} fontSize={250}>Test</Txt>
+            <Icon icon={'eos-icons:database'} size={820} color={secondary}/>
+        </Grid>
+    )
 
     yield* all(
-        circle().scale(2, 2).to(1, 2),
-        circle().skew(10, 1).to(1, 2),
+        grid().position.x(-1000, 1)
+        // view.fill(bg, 2) // animate it
+        // ().scale(2, 2).to(1, 2),
+        // circle().skew(10, 1).to(1, 2),
     )
-    yield*    circle().fill("#ffffff", 2).to("#e13238", 1);
+    yield* all(
+
+    )
+    // yield*    circle().fill("#ffffff", 2).to("#e13238", 1);
 });
